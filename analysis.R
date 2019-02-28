e302ffa3abd88e0408481b297b4f92f6a318c62c
Anna Krystalli <annakrystalli@googlemail.com>
2019-02-28 15:35:14 +0000

    add comments to script

--- analysis.R
+++ analysis.R
@@ -1,5 +1,13 @@
 library(dplyr)
-df <- data_frame(x = c(1, 1, 1, 2, 2), y = 1:5)
-df %>% distinct(x)
+# We have a data.frame of x & y values
+df <- data_frame(x = c(1, 1, 3, 3, 1, 2, 2, 1, 1, 3), y = 1:10)
+
+# to get the distinct values of x we use dplyr function distinct
+distinct <- df %>% distinct(x)
+distinct
+
+# to get only the values of x we subset the second column
+distinct_x <- distinct[,2]
+distinct_x
 
 sessionInfo()
