4986ab372fdfe37d0df01f82c907a5de4e7b009f
Anna Krystalli <annakrystalli@googlemail.com>
2019-02-28 15:39:17 +0000

    edit with latest version instructions

--- analysis.R
+++ analysis.R
@@ -1,5 +1,20 @@
 library(dplyr)
-df <- data_frame(x = c(1, 1, 1, 2, 2), y = 1:5)
-df %>% distinct(x)
+# We have a data.frame of x & y values
+df <- data_frame(x = c(1, 1, 3, 3, 1, 2, 2, 1, 1, 3), y = 1:10)
 
+# to get the distinct values of x we use dplyr function distinct
+distinct <- df %>% distinct(x)
+distinct
+
+# With this version of dplyr, the output of distinct has changed.
+# try running the next line of code. Does it work? Why not?
+distinct_x <- distinct[,2]
+distinct_x
+
+# check the version of loaded packages.
 sessionInfo()
+
+# Bonus exercises
+# - Can you rewrite the analysis code in the one year later environment to account 
+#   for the change in the function API
+# - Can you rewrite the analysis code so that it works for both versions?
